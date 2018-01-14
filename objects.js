9499ec4f8b1504a942054bed73817d05245f7451
David Chen <clfhhc@gmail.com>
2018-01-14 09:02:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
-var playlist={Ne-yo:"So sick"};
+var playlist={Neyo:"So sick"};
 
 function updatePlaylist(playlist,artistName,songTitle) {
   return object.assign{{},playlist,{[artistName]:songTitle}};
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
